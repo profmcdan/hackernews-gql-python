b7001d49b7645834ace24b75bd314230f8b70de4
Daniel Ale <danielale9291@gmail.com>
2019-10-18 10:29:27 +0100

    Add voting feature

--- links/models.py
+++ links/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 
@@ -6,6 +7,15 @@ from django.db import models
 class Link(models.Model):
     url = models.URLField()
     description = models.TextField(blank=True)
+    posted_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.url
+
+
+class Vote(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    link = models.ForeignKey(Link, related_name='votes',
+                             on_delete=models.CASCADE)
