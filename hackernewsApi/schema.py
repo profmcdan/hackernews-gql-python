10b125c3ccfa062c79f792ea69dacf388b45aaa4
Daniel Ale <danielale9291@gmail.com>
2019-10-18 10:06:05 +0100

    update authentication

--- hackernewsApi/schema.py
+++ hackernewsApi/schema.py
@@ -1,14 +1,17 @@
 import graphene
+import graphql_jwt
 import links.schema
 import users.schema
 
 
-class Query(links.schema.Query, graphene.ObjectType):
+class Query(users.schema.Query, links.schema.Query, graphene.ObjectType):
     pass
 
 
 class Mutation(users.schema.Mutation, links.schema.Mutation, graphene.ObjectType):
-    pass
+    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    verify_token = graphql_jwt.Verify.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
--- hackernewsApi/settings.py
+++ hackernewsApi/settings.py
@@ -123,4 +123,12 @@ STATIC_URL = '/static/'
 
 GRAPHENE = {
     'SCHEMA': 'hackernewsApi.schema.schema',
+    'MIDDLEWARE': [
+        'graphql_jwt.middleware.JSONWebTokenMiddleware',
+    ],
 }
+
+AUTHENTICATION_BACKENDS = [
+    'graphql_jwt.backends.JSONWebTokenBackend',
+    'django.contrib.auth.backends.ModelBackend',
+]
